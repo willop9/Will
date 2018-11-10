d40883de7f6e56f49923ef970c8ae84c946440dc
willop9 <willop9@googlemail.com>
2018-11-10 16:27:01 +0000

    Added 'api/courses/' route and created a deafault '/' route which disps 'helloworld'

--- Server.js
+++ Server.js
@@ -1,9 +1,18 @@
-var http = require('http');
+const http = require('http');
 
-var server = http.createServer(function(req, res){
+var server = http.createServer((req, res) =>{
+
   console.log('Got a request');
-  res.write('Hi');
-  console.log(req.url);
-  res.end();
+
+  if(req.url === '/'){
+    res.write('Hi');
+    console.log(req.url);
+    res.end();
+  }
+
+  if(req.url === '/api/courses'){
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
 });
 server.listen(3000);
