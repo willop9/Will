8e032e1da7d51775c15d2f1d49b937aefb90f40c
willop9 <willop9@googlemail.com>
2018-11-10 15:53:10 +0000

    Added line for console read

--- Server.js
+++ Server.js
@@ -3,6 +3,7 @@ var http = require('http');
 var server = http.createServer(function(req, res){
   console.log('Got a request');
   res.write('Hi');
+  console.log(req.url);
   res.end();
 });
 server.listen(3000);
